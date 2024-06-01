e61097a0c83e3171db69790e78a851c917cfeb5e
SlimeyAr <lostbhijaya@gmail.com>
2024-06-01 16:04:35 +0700

    Add Box2D

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
+
+import { browserslistToTargets } from 'lightningcss';
 
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), expressiveCode(), mdx()],
-  site: "https://slimeymc.github.com"
+  integrations: [wasm(), topLevelAwait(), alpinejs(), expressiveCode(), mdx()],
+  site: "https://slimeymc.github.com",
+  vite: {
+    plugins: [wasm(), topLevelAwait()],
+    css: {
+      transformer: "lightningcss"
+    },
+    build: {
+      cssMinify: 'lightningcss'
+    }
+  },
 });
