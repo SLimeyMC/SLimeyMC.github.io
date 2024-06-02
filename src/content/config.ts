b489bd713cd63b15c8a17a2553dcebf1a3d525a0
SlimeyAr <lostbhijaya@gmail.com>
2024-06-02 19:57:29 +0700

    feat: new blog schema; Image isntead of img

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,22 @@
-import { z, defineCollection } from 'astro:content';
+import {z, defineCollection, reference} from 'astro:content';
 
 const blogCollection = defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         createdAt: z.date(),
+        language: z.enum(['en-US', 'en-GB', 'id-ID']).optional().default('en-GB'),
         updatedAt: z.date().optional(),
-        heroImage: z.string().optional(),
+        heroSrc: image().optional(),
+        heroAlt: z.string().optional().default("placeholder; no image",
+        tags: z.array(z.string()),
+        relatedBlogs: z.array(reference('blog')).optional(),
+        footnote: z.string().optional(),
     }),
 });
 
+/* TODO get github user and it's commit date for the revision history */
 const wikiCollection = defineCollection({
     type: 'content',
     schema: z.object({
