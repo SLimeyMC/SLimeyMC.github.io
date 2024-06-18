4b7c09fab819047af43dcf7b3eee38b79aa08139
SlimeyAr <lostbhijaya@gmail.com>
2024-06-18 15:18:27 +0700

    feat: autlink heading and minimap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+
 import { browserslistToTargets } from 'lightningcss';
 
 import expressiveCode from "astro-expressive-code";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +24,9 @@ export default defineConfig({
     build: {
       cssMinify: 'lightningcss'
     }
+  },
+  markdown: {
+    rehypePlugins: [rehypeHeadingIds, rehypeAutolinkHeadings ]
   }
 });
 // find why rehypeslug doesnt work
